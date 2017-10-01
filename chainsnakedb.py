4bb1bde28703b1a445e91d2a7cc683054cd5232d
harrislin <harrislin_0_0@hotmail.com>
2017-10-01 05:27:24 -0700

    more db functions

--- chainsnakedb.py
+++ chainsnakedb.py
@@ -46,7 +46,6 @@ class ChainsDb(object):
 
         card_table_query = """CREATE TABLE CARD (
             C_ID INT AUTO_INCREMENT PRIMARY KEY,
-            K_ID INT,
             C_QUESTION VARCHAR(256),
             C_ANSWER VARCHAR(256)
             );"""
@@ -56,6 +55,7 @@ class ChainsDb(object):
         c_kw_relationship_table_query = """CREATE TABLE C_KW_RELATIONSHIPS (
             K_ID INT,
             C_ID INT,
+            WEIGHT DOUBLE,
             CONSTRAINT PK_C_KW_RELATIONSHIP PRIMARY KEY (C_ID, K_ID),
             CONSTRAINT FK_CARD
                 FOREIGN KEY (C_ID) REFERENCES CARD (C_ID),
@@ -132,12 +132,133 @@ class ChainsDb(object):
         # disconnect from server
         db.close()
 
+    def insert_to_card(self, question, answer):
+        # Open database connection
+        db = psql.connect(config.host,config.dbusr,config.dbpwd,"chains" )
 
+        # prepare a cursor object using cursor() method
+        cursor = db.cursor()
 
+        insert_query = """INSERT INTO CARD (
+                            C_QUESTION, C_ANSWER ) VALUES (
+                            '%s', '%s')""" % (question, answer)
+
+        cursor.execute(insert_query)
+        db.commit()
+
+        db.close()
+
+    def insert_to_c_kw(self, question, word, weight):
+        # Open database connection
+        db = psql.connect(config.host,config.dbusr,config.dbpwd,"chains" )
+
+        # prepare a cursor object using cursor() method
+        cursor = db.cursor()
+
+        q_select_query = """SELECT C_ID FROM CARD
+                             WHERE C_QUESTION =
+                            '%s'""" % (question)
+        cursor.execute(q_select_query)
+        data = cursor.fetchone()
+        c_id = data[0]
+
+        w_select_query = """SELECT K_ID FROM KEY_WORDS
+                             WHERE K_NAME =
+                            '%s'""" % (word)
+        cursor.execute(w_select_query)
+        data = cursor.fetchone()
+        w_id = data[0]
+
+        c_kw_insert_query = """INSERT INTO C_KW_RELATIONSHIPS (
+                              C_ID, K_ID, WEIGHT) VALUES (
+                              %d, %d, %f)""" % (c_id, w_id, weight)
+        cursor.execute(c_kw_insert_query)
+        db.commit()
+
+        # disconnect from server
+        db.close()
+
+    def get_questions_given_keyword(self, word):
+        # Open database connection
+        db = psql.connect(config.host,config.dbusr,config.dbpwd,"chains" )
+
+        # prepare a cursor object using cursor() method
+        cursor = db.cursor()
+
+        k_select_query = """SELECT K_ID FROM KEY_WORDS
+                             WHERE K_NAME =
+                            '%s'""" % (word)
+        cursor.execute(k_select_query)
+        data = cursor.fetchone()
+        k_id = data[0]
+
+        q_select_query = """SELECT CARD.C_QUESTION FROM CARD 
+                             INNER JOIN C_KW_RELATIONSHIPS
+                             ON CARD.C_ID = C_KW_RELATIONSHIPS.C_ID
+                             INNER JOIN KEY_WORDS
+                             ON C_KW_RELATIONSHIPS.K_ID = KEY_WORDS.K_ID
+                             WHERE C_KW_RELATIONSHIPS.WEIGHT > 0.7 AND C_KW_RELATIONSHIPS.K_ID = %i
+                             ORDER BY C_KW_RELATIONSHIPS.WEIGHT DESC;""" % (k_id)
+
+        cursor.execute(q_select_query)
+        data = cursor.fetchall()
+
+        try:
+            if data[0] is not None: questions = data
+        except:
+            questions = {}
+        
+        # disconnect from server
+        db.close()
+
+        return questions
+
+    def get_keyword_given_question(self, question):
+        # Open database connection
+        db = psql.connect(config.host,config.dbusr,config.dbpwd,"chains" )
+
+        # prepare a cursor object using cursor() method
+        cursor = db.cursor()
+
+        q_select_query = """SELECT C_ID FROM CARD
+                             WHERE C_QUESTION =
+                            '%s'""" % (question)
+        cursor.execute(q_select_query)
+        data = cursor.fetchone()
+        c_id = data[0]
+
+        k_select_query = """SELECT KEY_WORDS.K_NAME FROM KEY_WORDS 
+                             INNER JOIN C_KW_RELATIONSHIPS
+                             ON KEY_WORDS.K_ID = C_KW_RELATIONSHIPS.K_ID
+                             INNER JOIN CARD
+                             ON C_KW_RELATIONSHIPS.C_ID = CARD.C_ID
+                             WHERE C_KW_RELATIONSHIPS.WEIGHT > 0.7 AND C_KW_RELATIONSHIPS.C_ID = %i
+                             ORDER BY C_KW_RELATIONSHIPS.WEIGHT DESC;""" % (c_id)
+
+        cursor.execute(k_select_query)
+        data = cursor.fetchall()
+
+        try:
+            if data[0] is not None: words = data
+        except:
+            words = {}
+        
+        # disconnect from server
+        db.close()
+
+        return words
 
 chainsDb = ChainsDb()
 chainsDb.reset_db()
 chainsDb.insert_to_topics("SOLAR SYSTEMS")
 chainsDb.insert_to_key_word("cell barrier")
+chainsDb.insert_to_key_word("woofers")
 chainsDb.insert_to_relationships("cell barrier", "SOLAR SYSTEMS", 0.22)
+chainsDb.insert_to_card("How old is trent really?", "probably like forty years old")
+chainsDb.insert_to_card("How sexy are you?", "very very very sexy")
+chainsDb.insert_to_c_kw("How old is trent really?", "cell barrier", 0.891)
+chainsDb.insert_to_c_kw("How sexy are you?", "cell barrier", 0.701)
+chainsDb.insert_to_c_kw("How old is trent really?", "woofers", 0.791)
+chainsDb.get_questions_given_keyword("cell barrier")
+chainsDb.get_keyword_given_question("How old is trent really?")
 print "meow"
