6cb234237e8fc4df7db7e043a6b1c58b38037158
harrislin <harrislin_0_0@hotmail.com>
2017-10-01 11:22:33 -0700

    changed api to format

--- chainsnake.py
+++ chainsnake.py
@@ -10,26 +10,28 @@ app = Flask(__name__)
 def fetch():
     set_to_fetch = request.args.get('fetch')
     chaindb = chainsnakedb.ChainsDb()
-    response = chaindb.get_questions_given_keyword('cell barrier')
+    response = chaindb.get_questions_given_question("The chairman of China")
     response["topic"] = set_to_fetch
-    response["description"] = "Animals and natural reproduction"
+    response["description"] = "Information about important figures in political history of China"
     return json.dumps(response)
 
 @app.route('/hello/<u_name>')
 def address(u_name):
     return 'hey {}! did you know that harris is gay'.format(u_name)
 
-@app.route('/word')
+@app.route('/getnext')
 def question():
     key = request.args.get('key')
     chaindb = chainsnakedb.ChainsDb()
-    return json.dumps(chaindb.get_questions_given_keyword(key))
+    response = chaindb.get_questions_given_question(key)
+    return json.dumps(response)
 
 @app.route('/question')
 def word():
     key = request.args.get('key')
     chaindb = chainsnakedb.ChainsDb()
-    return json.dumps(chaindb.get_keyword_given_question(key))
+    response = chaindb.get_keyword_given_question(key)
+    return json.dumps(response)
 
 @app.route('/')
 def index():
