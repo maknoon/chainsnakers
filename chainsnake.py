3d43c40cfa318bfee8c3bb9031b7ccdf9b52930b
maknoon <conniemob@gmail.com>
2017-10-01 04:29:26 -0700

    lambda connected to API

--- chainsnake.py
+++ chainsnake.py
@@ -1,12 +1,19 @@
 #!~/usr/bin/python3
-from flask import Flask
+from flask import Flask, request
 import pymysql
+import json
 
 app = Flask(__name__)
 
+@app.route('/setup')
+def fetch():
+    set_to_fetch = request.args.get('fetch')
+
+    return json.dumps({"message": "fetched the {} set!".format(set_to_fetch)},indent=2)
+
 @app.route('/hello/<u_name>')
 def address(u_name):
-	return 'hey {}! did you know that harris is gay'.format(u_name)
+    return 'hey {}! did you know that harris is gay'.format(u_name)
 
 @app.route('/')
 def index():
