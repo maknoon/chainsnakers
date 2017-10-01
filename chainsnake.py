c245de1cbc1d1e46a3cead17e2f0cfbab5c0c5a7
maknoon <conniemob@gmail.com>
2017-09-30 18:08:28 -0700

    added watson

--- chainsnake.py
+++ chainsnake.py
@@ -1,8 +1,20 @@
 #!~/usr/bin/python3
 from flask import Flask
 import pymysql
+import json
+import config
+from watson_developer_cloud import NaturalLanguageUnderstandingV1
+import watson_developer_cloud.natural_language_understanding.features.v1 as Features
 
 app = Flask(__name__)
+sem = NaturalLanguageUnderstandingV1(
+  username=config.watson_u,
+  password=config.watson_p,
+  version=config.watson_d)
+
+@app.route('/test_watson')
+def test_watson():
+	return 'connected'
 
 @app.route('/hello/<u_name>')
 def address(u_name):
