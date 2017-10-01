b60c1e0c98872059599779039dcdce51460e00cb
maknoon <conniemob@gmail.com>
2017-10-01 12:15:31 -0700

    lambda poc complete

--- lmbd/chainsnake_lambda.py
+++ lmbd/chainsnake_lambda.py
@@ -2,22 +2,33 @@
 amazon lambda sysquiz function
 """
 from __future__ import print_function
-import os, urllib2, json
+import os, urllib, urllib2, json
 
 # --------------- env vars + some strings -> API queries ----------------------
 snake_srvr_ip = os.environ["SNAKE_SRVR"]
-fetch_new_set_url = "{0}/setup?fetch={1}"
+fetch_new_set_url = "{0}/setup?{1}"
+fetch_getnext_url = "{0}/getnext?{1}"
 
 # --------------- HERE ARE THE HELPER CALLS TO SNAKE./.. ----------------------
-# returns a JSON object of sets from the SNAKE SERVER
+# corresponds to "TEACH ME ABOUT <SYSTEM>"
 def get_set_from_srvr(system_to_fetch):
-    url = fetch_new_set_url.format(snake_srvr_ip,system_to_fetch)
+    encoded_system = urllib.urlencode({'fetch': system_to_fetch})
+    url = fetch_new_set_url.format(snake_srvr_ip,encoded_system)
+
     response = urllib2.urlopen(url)
     data = dict(json.loads(response.read()))
-    msg = data['message']
 
-    return msg
+    return data
 
+# corresponds to "WHAT'S NEXT" / "COULD YOU REPEAT THAT" / "WHAT'S SIMILAR"
+def get_term_from_srvr(term_to_fetch):
+    encoded_term = urllib.urlencode({'key': term_to_fetch})
+    url = fetch_getnext_url.format(snake_srvr_ip,encoded_term)
+
+    response = urllib2.urlopen(url)
+    data = dict(json.loads(response.read()))
+
+    return data
 
 # --------------- Helpers that build all of the responses ----------------------
 def build_speechlet_response(title, output, reprompt_text, should_end_session):
@@ -72,7 +83,7 @@ def get_welcome_response():
 
 def handle_session_end_request():
     card_title = "Session Ended"
-    speech_output = "Thank you for trying the System Learning with Alexa. " \
+    speech_output = "Thank you for trying the System Learning Module with Alexa. " \
                     "Have a nice day! "
     # Setting this to true ends the session and exits the skill.
     should_end_session = True
@@ -80,8 +91,9 @@ def handle_session_end_request():
         card_title, speech_output, None, should_end_session))
 
 
-def create_system_set_attributes(system_set):
-    return {"systemSet": system_set}
+def create_attributes(system_set,current_term,current_def):
+    return {"systemSet": system_set, "currentTerm": current_term,
+            "currentDef": current_def}
 
 
 def set_system_in_session(intent, session):
@@ -95,19 +107,31 @@ def set_system_in_session(intent, session):
 
     if 'System' in intent['slots']:
         system_set = intent['slots']['System']['value']
-        session_attributes = create_system_set_attributes(system_set)
 
         # API call to fetch & generate the set of cards
         srvr_resp = get_set_from_srvr(system_set)
+        topic_resp_message = "I have fetched {} data, ".format(srvr_resp['topic'])
+        keywords = srvr_resp['results']
+        session_attributes = create_attributes(system_set, keywords[0]['term'],
+                                keywords[0]['definition'])
 
-        speech_output = srvr_resp + \
+        speech_output = topic_resp_message + \
                         "Let's begin learning about " + \
-                        system_set + \
-                        ". You can ask me more about " + \
+                        system_set + " , " + \
+                        srvr_resp['description'] + \
+                        ". Some " + system_set + " related keywords are, " + \
+                        keywords[0]['term'] + ", and, " + \
+                        keywords[1]['term'] + "." + \
+                        " You can ask me more about " + \
                         system_set + \
                         " by asking, " \
                         "what's next?"
-        reprompt_text = "You can ask me to repeat by saying, " \
+
+        reprompt_text = " You can ask me more about " + \
+                        system_set + \
+                        " by asking, " \
+                        "what's next? Or, " + \
+                        "You can ask me to repeat by saying, " \
                         "could you repeat"
     else:
         speech_output = "I'm not sure what system you are talking about. " \
@@ -119,13 +143,51 @@ def set_system_in_session(intent, session):
         card_title, speech_output, reprompt_text, should_end_session))
 
 
+def go_to_another_term(intent, session):
+    session_attributes = {}
+    should_end_session = False
+
+    if session.get('attributes', {}) and "currentTerm" in session.get('attributes', {}):
+        system_set = session['attributes']['systemSet']
+        current_term = session['attributes']['currentTerm']
+        current_definition = session['attributes']['currentDef']
+
+        srvr_resp = get_term_from_srvr(current_definition)
+        keywords = srvr_resp['results']
+        session_attributes = create_attributes(system_set, keywords[0]['term'],
+                                keywords[0]['definition'])
+
+        speech_output = "The current term is " + current_term + \
+                        ". " + current_term + " is " + current_definition + \
+                        ". Terms similar to " + current_term + " include, " + \
+                        keywords[0]['term'] + ", and " + \
+                        keywords[1]['term'] + "."
+
+        reprompt_text = " You can ask me more about " + \
+                        system_set + \
+                        " by asking, " \
+                        "what's next? Or, " + \
+                        "You can ask me to repeat by saying, " \
+                        "could you repeat"
+    else:
+        speech_output = "I'm not sure what system you are talking about. " \
+                        "You can say, teach me about biology."
+        should_end_session = False
+
+    # Setting reprompt_text to None signifies that we do not want to reprompt
+    # the user. If the user does not respond or says something that is not
+    # understood, the session will end.
+    return build_response(session_attributes, build_speechlet_response(
+        intent['name'], speech_output, reprompt_text, should_end_session))
+
+
 def get_system_from_session(intent, session):
     session_attributes = {}
     reprompt_text = None
 
     if session.get('attributes', {}) and "systemSet" in session.get('attributes', {}):
         system_set = session['attributes']['systemSet']
-        speech_output = "The system we are studying is " + system_set + \
+        speech_output = "The system we have studied is " + system_set + \
                         ". Goodbye."
         should_end_session = True
     else:
@@ -173,10 +235,12 @@ def on_intent(intent_request, session):
     if intent_name == "FetchNewSystemIntent":
         return set_system_in_session(intent, session)
     elif intent_name == "WhatsNextIntent":
-        return get_system_from_session(intent, session)
+        return go_to_another_term(intent, session)
     elif intent_name == "WhatsBeforeIntent":
-        return get_system_from_session(intent, session)
+        return go_to_another_term(intent, session)
     elif intent_name == "WhatsSimilarIntent":
+        return go_to_another_term(intent, session)
+    elif intent_name == "ExitProgramIntent":
         return get_system_from_session(intent, session)
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
