46d27258e25adfaf2540d4657d7c68aae20fe525
Joy <joy8zhang@gmail.com>
2017-09-30 22:56:06 -0700

    watson on quizlet set using URL

--- chainsnake.py
+++ chainsnake.py
@@ -1,6 +1,6 @@
 #!~/usr/bin/python3
-from flask import Flask, request
-import pymysql
+from flask import Flask
+#import pymysql
 import json
 import config
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
@@ -11,32 +11,61 @@ sem = NaturalLanguageUnderstandingV1(
   username=config.watson_u,
   password=config.watson_p,
   version=config.watson_d)
-
+# return answer and question keywords
 @app.route('/test_watson')
 def test_watson():
-	return 'connected'
 
-@app.route('/setup')
-def fetch():
-    set_to_fetch = request.args.get('fetch')
+    text = "An organization of workers that tries to improve working conditions, wages, and benefits for its members"
+    if len(text) < 15:
+        answer = text
+    else:
+        answer = sem.analyze(
+            text="An organization of workers that tries to improve working conditions, wages, and benefits for its members",
+            features=[
+                Features.Keywords(
+                # Semantic Roles options
+                )
+            ]
+        )
+    text="Social Justice"
+    if len(text) < 15:
+        question = text
+    else:
+        question = sem.analyze(
+            text="Social Justice",
+            features=[
+                Features.Keywords(
+                # Semantic Roles options
+                )
+            ]
+        )
+    return(json.dumps(answer, indent=2))
+    return(json.dumps(question, indent=2))
+#return single search term given alexa question
+@app.route('/test_searchterm')
+def text_searchterm():
+    text = "tell me about social justice and obama?"
+    if len(text) < 15:
+        keywords = text
+    else:
+        keywords = sem.analyze(
+            text="tell me about social Justice and obama",
+            features=[
+                Features.Keywords(
+                # Semantic Roles options
+                )
+            ]
+        )
 
-    return json.dumps({"message": "fetched the {} set!".format(set_to_fetch)},indent=2)
+    dictionary = (json.dumps(keywords, indent=2))
+    keyword = keywords["keywords"][0]["text"]
+    return(keyword)
+
+
 
 @app.route('/hello/<u_name>')
 def address(u_name):
     return 'hey {}! did you know that harris is gay'.format(u_name)
-
-@app.route('/word')
-def question():
-    key = request.args.get('key')
-    chaindb = chainsnakedb.ChainsDb()
-    return json.dumps(chaindb.get_questions_given_keyword(key))
-
-@app.route('/question')
-def word():
-    key = request.args.get('key')
-    chaindb = chainsnakedb.ChainsDb()
-    return json.dumps(chaindb.get_keyword_given_question(key))
 
 @app.route('/')
 def index():
