a29ea906be020fba3a72095a4da3524f4b95fd1d
Joy <joy8zhang@gmail.com>
2017-10-01 06:22:19 -0700

    added alexa question to quizlet keyword implementation

--- chainsnake.py
+++ chainsnake.py
@@ -11,30 +11,55 @@ sem = NaturalLanguageUnderstandingV1(
   username=config.watson_u,
   password=config.watson_p,
   version=config.watson_d)
-
+# return answer and question keywords
 @app.route('/test_watson')
 def test_watson():
-    question = sem.analyze(
-        text="",
-        features=[
-            Features.Keywords(
-            # Semantic Roles options
-            )
-        ]
-    )
 
-    answer = sem.analyze(
-        text="",
-        features=[
-            Features.Keywords(
-            # Semantic Roles options
-            )
-        ]
-    )
-    print(json.dumps(question, indent=2))
-    print(json.dumps(answer, indent=2))
+    text = "An organization of workers that tries to improve working conditions, wages, and benefits for its members"
+    if len(text) < 15:
+        answer = text
+    else:
+        answer = sem.analyze(
+            text="An organization of workers that tries to improve working conditions, wages, and benefits for its members",
+            features=[
+                Features.Keywords(
+                # Semantic Roles options
+                )
+            ]
+        )
+    text="Social Justice"
+    if len(text) < 15:
+        question = text
+    else:
+        question = sem.analyze(
+            text="Social Justice",
+            features=[
+                Features.Keywords(
+                # Semantic Roles options
+                )
+            ]
+        )
+    return(json.dumps(answer, indent=2))
+    return(json.dumps(question, indent=2))
+#return single search term given alexa question
+@app.route('/test_searchterm')
+def text_searchterm():
+    text = "tell me about social justice and obama?"
+    if len(text) < 15:
+        keywords = text
+    else:
+        keywords = sem.analyze(
+            text="tell me about social Justice and obama",
+            features=[
+                Features.Keywords(
+                # Semantic Roles options
+                )
+            ]
+        )
 
-
+    dictionary = (json.dumps(keywords, indent=2))
+    keyword = keywords["keywords"][0]["text"]
+    return(keyword)
 
 
 
